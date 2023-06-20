a9dd48cf223cff824aa37a2c069b66795f73ccc6
DuelistRag3 <christian.dullin@web.de>
2023-06-20 23:07:48 +0200

    Update install_vs_code.sh

--- dev_jammy/src/ubuntu/install/vs_code/install_vs_code.sh
+++ dev_jammy/src/ubuntu/install/vs_code/install_vs_code.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
-set -ex
-ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/x64/g')
-wget -q https://update.code.visualstudio.com/latest/linux-deb-${ARCH}/stable -O vs_code.deb
+wget -q https://az764295.vo.msecnd.net/stable/695af097c7bd098fbf017ce3ac85e09bbc5dda06/code_1.79.2-1686734195_amd64.deb -O vs_code.deb
+mv 'vs_code.deb'$'\r' vs_code.dep
 apt-get update
-apt-get install -y ./vs_code.deb
-mkdir -p /usr/share/icons/hicolor/apps
-wget -O /usr/share/icons/hicolor/apps/vscode.svg https://kasm-static-content.s3.amazonaws.com/icons/vscode.svg
-sed -i '/Icon=/c\Icon=/usr/share/icons/hicolor/apps/vscode.svg' /usr/share/applications/code.desktop
-sed -i 's#/usr/share/code/code#/usr/share/code/code --no-sandbox##' /usr/share/applications/code.desktop
-cp /usr/share/applications/code.desktop $HOME/Desktop
-chmod +x $HOME/Desktop/code.desktop
-chown 1000:1000 $HOME/Desktop/code.desktop
-rm vs_code.deb
+dpkg -i ./vs_code.deb
 
 # Conveniences for python development
 apt-get update
