fa79b20ab8edbe987d831aa561f086190b339048
DuelistRag3 <christian.dullin@web.de>
2023-06-30 21:22:29 +0200

    add mariadb and fix vscode

--- dev_jammy/src/ubuntu/install/vs_code/install_vs_code.sh
+++ dev_jammy/src/ubuntu/install/vs_code/install_vs_code.sh
@@ -1,19 +1,9 @@
-#!/usr/bin/env bash
-wget -q https://az764295.vo.msecnd.net/stable/695af097c7bd098fbf017ce3ac85e09bbc5dda06/code_1.79.2-1686734195_amd64.deb -O vs_code.deb
-mv 'vs_code.deb'$'\r' vs_code.dep
-apt-get update
-dpkg -i ./vs_code.deb
+apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 
-# Conveniences for python development
-apt-get update
-apt-get install -y python3-setuptools \
-                   python3-venv \
-                   python3-virtualenv
-
-# Cleanup
-if [ -z ${SKIP_CLEAN+x} ]; then
-  apt-get autoclean
-  rm -rf \
-    /var/lib/apt/lists/* \
-    /var/tmp/*
-fi
+apt install apt-transport-https
+apt update
+apt install code
